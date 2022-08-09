bc5fc5faaf11807fa40e59bc74dd528e06a0d257
Jilles Wielaard <jilles.wielaard@kpn.com>
2022-08-09 13:33:25 +0200

    added enums

---
+++ src/enums/index.ts
@@ -0,0 +1,12 @@
+export enum ScheduleState {
+  Active = 'Active',
+  LoggedIn = 'LoggedIn',
+  LoggedOff = 'LoggedOff',
+  NotFound = 'NotFound',
+  Error = 'Error',
+}
+
+export enum UserStatus {
+  Available = '6a3af858-942f-489d-9700-5f9bcdcdae9b',
+  Offline = 'ccf3c10a-aa2c-4845-8e8d-f59fa48c58e5',
+}
